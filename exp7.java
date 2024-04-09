76c3701d7d2ef44fda89bd5a902993b5b88d8d28
Kashish <103774833+Kashish006@users.noreply.github.com>
2024-04-09 23:52:27 +0530

    Update exp7.java

--- exp7.java
+++ exp7.java
@@ -9,5 +9,25 @@ package DAA;
  * @author Kashish
  */
 public class exp7 {
+    public static void main(String[] args) {
+      
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the string to be repeated: ");
+        String s = in.next();
+        System.out.print("enter the number of characters to be considered: ");
+        long n = in.nextLong();
+        long num = n/s.length();
+        long rem = n%s.length();
+        long ans = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i)=='a') {
+                ans += num;
+                if (i < rem)
+                    ans++;
+            }
+        }
+        System.out.println(ans);
+    }
     
 }
